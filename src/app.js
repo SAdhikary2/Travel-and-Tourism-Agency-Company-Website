2298ef4c04252bae4178dc717e2eda5c69ca97c6
sukalyan Adhikary <sadhikary0055@gmail.com>
2023-10-14 09:28:27 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,50 @@
+require("dotenv").config();
 const express=require('express');
 const app=express();
 const path=require('path')
 const hbs=require('hbs')
-// const collection=require("./mongodb")
+const bodyparser=require('body-parser')
+const mongoose=require('mongoose')
 const port=process.env.PORT || 3000;
+
+
+
+
+// MONGODB CONNECT 
+mongoose.connect('mongodb://127.0.0.1:27017/UserData', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>console.log('connected successfully'))
+.catch((err)=> console.error(err))
+//DEFINE USER SCHEMA AND MODEL
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+  });
+  const User = mongoose.model('User', userSchema);
+
+  // Use body-parser to parse request bodies
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// Handle POST request for /login
+app.post('/login', async (req, res) => {
+    const { loginname, loginpassword } = req.body;
+    const user = await User.findOne({ username: loginname, password: loginpassword });
+    if (user) {
+      res.render('index');
+    } else {
+      res.send('Invalid credentials');
+    }
+  });
+  
+  
+  // Handle registration form submissions
+app.post('/signup', async (req, res) => {
+    const { username, email, password } = req.body;
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.render('booking')
+   
+  });
 
 
 // PATH 
@@ -87,6 +128,52 @@ app.get('/africa',(req,res)=>{
 app.get('/login',(req,res)=>{
     res.render('login')
 })
+
+app.get('/signup',(req,res)=>{
+    res.render('login')
+})
+
+
+
+// document.getElementById('bookingButton').addEventListener('click', function() {
+//     fetch('/checkLoginStatus')
+//     .then(response => response.json())
+//     .then(data => {
+//         if (data.loggedIn) {
+//             window.location.href = '/booking';
+//         } else {
+//             window.location.href = '/login';
+//         }
+//     });
+// });
+
+// app.get('/checkLoginStatus', (req, res) => {
+//     if (req.session && req.session.loggedIn) {
+//         res.json({ loggedIn: true });
+//     } else {
+//         res.json({ loggedIn: false });
+//     }
+// });
+
+
+
+function checkUserLoggedIn(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+app.get('/booking', checkUserLoggedIn, (req, res) => {
+    // If the user is logged in, this code will be executed
+    res.redirect('/bookingPage');
+});
+
+
+
+
+
 
 
 
